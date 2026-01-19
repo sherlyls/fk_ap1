36dc1f336e390ff187fcc5eb55c4ee79fc51251a
sherlyls <sherlyls862@gmail.com>
2026-01-19 11:49:49 +0700

    update

--- fk1/src/api/products.js
+++ fk1/src/api/products.js
@@ -1,12 +1,18 @@
-const BASE_URL = 'https://dummyjson.com/products'
+const BASE_URL = "https://dummyjson.com/products";
 
+export async function fetchProducts({ limit = 10, skip = 0, search = "" }) {
+  let url = `${BASE_URL}?limit=${limit}&skip=&{skip}`;
 
-export async function fetchProducts({ limit = 10, skip = 0}) {
-    const res = await fetch(
-        `${BASE_URL}?limit=${limit}&skip=${skip}`
-    )
-    if(!res.ok) {
-        throw new Error('Gagal mengambil produk')
-    } 
-    return res.json()
+  if (search) {
+    url = `${BASE_URL}/search?q=${encodeURIComponent(
+      search
+    )}&limit=${limit}&skip=${skip}`;
+  }
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("Gagal mengambil produk");
+  }
+  return res.json();
 }
--- fk1/src/pages/Home.jsx
+++ fk1/src/pages/Home.jsx
@@ -1,35 +1,110 @@
-import {useEffect, useState} from 'react'
-import {fetchProducts} from '../api/products'
+import { useState, useEffect } from "react";
+import { fetchProducts } from "../api/products";
 
-export default function Home () {
+export default function Home() {
+  const LIMIT = 10;
 
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [skip, setSkip] = useState(0);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [search, setSearch] = useState("");
+  const [searchInput, setSearchInput] = useState("")
 
-    useEffect(() => {
-        async function load() {
-            try {
-                const result = await fetchProducts({limit: 10, skip: 0})
-                setData(result)
-            }
-            catch (err) {
-                setError(err.message)
-            }
-            finally {
-                setLoading(false)
-            }
-        }
-        load()
-    }, [])
+  const hasMore = products.length < total;
 
-    if(loading) return <p>Loading...</p>
-    if(error) return <p>{error}</p>
+  useEffect(() => {
+    async function load() {
+      try {
+        const result = await fetchProducts({ limit: LIMIT, skip: 0 });
+        setProducts(result.products);
+        setTotal(result.total);
+        setSkip(result.products.length);
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    load();
+  }, []);
 
-    return (
-        <div>
-        <h1>Products</h1>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-    )
+  async function loadMore() {
+    if (loadingMore) return;
+
+    setLoadingMore(true);
+
+    try {
+      const result = await fetchProducts({
+        limit: LIMIT,
+        skip: skip,
+      });
+      setProducts((prev) => [...prev, ...result.products]);
+      setSkip((prev) => prev + result.products.length);
+    } catch (err) {
+      console.log(err, "see error");
+      setError(err.message);
+    } finally {
+      setLoadingMore(false);
+    }
+  }
+
+  function resetPagination() {
+    setProducts([]);
+    setSkip(0);
+    setTotal(0);
+  }
+
+  useEffect(() => {
+    if (search === "") return;
+
+    async function searchProducts() {
+      setLoading(true);
+      resetPagination()
+
+      const result = await fetchProducts({ limit: LIMIT, skip: 0, search})
+
+      setProducts(result.products)
+      setTotal(result.total)
+      setSkip(result.products.length)
+      setLoading(false)
+    }
+
+    searchProducts()
+  }, [search]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error...</div>;
+
+  return (
+    <div>
+      <div>Home</div>
+
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      ></input>
+      <button onClick={() => setSearch(searchInput)}>
+Search
+      </button>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+
+      {hasMore ? (
+        <button onClick={loadMore} disabled={loadingMore}>
+          {loadingMore ? "Loading..." : "Load More"}
+        </button>
+      ) : (
+        <p>No more products</p>
+      )}
+    </div>
+  );
 }
